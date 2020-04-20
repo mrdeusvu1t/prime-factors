b00878c61eaf18a12168a94c3933960109944e9e
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-04-20 17:07:51 +0300

    add-prime-factors-task

---
+++ PrimeFactorsTask.Tests/PrimeFactorsTests.cs
@@ -0,0 +1,27 @@
+using System;
+using NUnit.Framework;
+
+namespace PrimeFactorsTask.Tests
+{
+    public class PrimeFactorsTests
+    {
+        [TestCase(1, ExpectedResult = new int[] { })]
+        [TestCase(2, ExpectedResult = new[] {2})]
+        [TestCase(9, ExpectedResult = new[] {3, 3})]
+        [TestCase(8, ExpectedResult = new[] {2, 2, 2})]
+        [TestCase(12, ExpectedResult = new[] {2, 2, 3})]
+        [TestCase(901255, ExpectedResult = new[] {5, 17, 23, 461})]
+        [TestCase(93819012551, ExpectedResult = new[] {11, 9539, 894119})]
+        public int[] GetFactorsTests(long number)
+        {
+            return PrimeFactors.GetFactors(number);
+        }
+
+        [TestCase(-100)]
+        [TestCase(-1)]
+        [TestCase(0)]
+        public static void GetFactorsTests_Throw_ArgumentOutOfRangeException_If_Number_Less_Or_Equals_Zero(long number)
+            => Assert.Throws<ArgumentOutOfRangeException>(() =>
+                PrimeFactors.GetFactors(number), $"For {number} must be throw ArgumentOutOfRangeException.");
+    }
+}
---
+++ PrimeFactorsTask/PrimeFactors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeFactorsTask
+{
+    public static class PrimeFactors
+    {
+        /// <summary>
+        /// Compute the prime factors of a given natural number.
+        /// A prime number is only evenly divisible by itself and 1.
+        /// Note that 1 is not a prime number.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>Prime factors of a given natural number.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Number less or equal zero.</exception>
+        /// <example>
+        /// 60 => {2, 2, 3, 5}
+        /// 8 => {2, 2, 2}
+        /// 12 => {2, 2, 3}
+        /// 901255 => {5, 17, 23, 461}
+        /// 93819012551 => {11, 9539, 894119}
+        /// </example>
+        public static int[] GetFactors(long number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
