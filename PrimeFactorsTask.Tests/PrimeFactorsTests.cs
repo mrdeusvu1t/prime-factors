62fa0d187e4214d59880c8d985fc9d5762a4fd72
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-29 17:48:15 +0000

    Replace PrimeFactorsTests.cs

--- PrimeFactorsTask.Tests/PrimeFactorsTests.cs
+++ PrimeFactorsTask.Tests/PrimeFactorsTests.cs
@@ -1,18 +1,20 @@
 using System;
 using NUnit.Framework;
 
+#pragma warning disable CA1707
+
 namespace PrimeFactorsTask.Tests
 {
     public class PrimeFactorsTests
     {
         [TestCase(1, ExpectedResult = new int[] { })]
-        [TestCase(2, ExpectedResult = new[] {2})]
-        [TestCase(9, ExpectedResult = new[] {3, 3})]
-        [TestCase(8, ExpectedResult = new[] {2, 2, 2})]
-        [TestCase(12, ExpectedResult = new[] {2, 2, 3})]
-        [TestCase(901255, ExpectedResult = new[] {5, 17, 23, 461})]
-        [TestCase(93819012551, ExpectedResult = new[] {11, 9539, 894119})]
-        [TestCase(342324, ExpectedResult = new [] {2, 2, 3, 3, 37, 257})]
+        [TestCase(2, ExpectedResult = new[] { 2 })]
+        [TestCase(9, ExpectedResult = new[] { 3, 3 })]
+        [TestCase(8, ExpectedResult = new[] { 2, 2, 2 })]
+        [TestCase(12, ExpectedResult = new[] { 2, 2, 3 })]
+        [TestCase(901255, ExpectedResult = new[] { 5, 17, 23, 461 })]
+        [TestCase(93819012551, ExpectedResult = new[] { 11, 9539, 894119 })]
+        [TestCase(342324, ExpectedResult = new[] { 2, 2, 3, 3, 37, 257 })]
         public int[] GetFactors_ReturnsPrimeFactors(long number)
         {
             return PrimeFactors.GetFactors(number);
@@ -21,8 +23,8 @@ namespace PrimeFactorsTask.Tests
         [TestCase(-100)]
         [TestCase(-1)]
         [TestCase(0)]
-        public static void GetFactors_IfNumberLessOrEqualsZero_ThrowArgumentException(long number)
-            => Assert.Throws<ArgumentException>(() =>
-                PrimeFactors.GetFactors(number), $"{number} cannot be less or equals zero.");
+        public void GetFactors_IfNumberLessOrEqualsZero_ThrowArgumentException(long number)
+            => Assert.Throws<ArgumentException>(
+                () => PrimeFactors.GetFactors(number), $"{number} cannot be less or equals zero.");
     }
 }
